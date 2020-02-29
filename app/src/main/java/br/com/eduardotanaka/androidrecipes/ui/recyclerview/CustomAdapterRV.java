6a673a7f771f41bb962e57cef0c8b063cc6b5de6
Eduardo <eduardo.s.tanaka@gmail.com>
2020-02-29 17:36:38 -0300

    vertical recyclerview

---
+++ app/src/main/java/br/com/eduardotanaka/androidrecipes/ui/recyclerview/CustomAdapterRV.java
@@ -0,0 +1,81 @@
+package br.com.eduardotanaka.androidrecipes.ui.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import br.com.eduardotanaka.androidrecipes.R;
+import br.com.eduardotanaka.androidrecipes.ui.retrofit.model.MockApi;
+
+public class CustomAdapterRV extends RecyclerView.Adapter {
+
+    private List<MockApi> mockApiList;
+    private static ItemClickListener itemClickListener;
+
+    public CustomAdapterRV(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView id;
+        private TextView name;
+        private TextView createdAt;
+        private LinearLayout linearLayout;
+
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            linearLayout = itemView.findViewById(R.id.rvLayout);
+            id = itemView.findViewById(R.id.rvId);
+            name = itemView.findViewById(R.id.rvName);
+            createdAt = itemView.findViewById(R.id.rvCreatedAt);
+        }
+
+        public void bindData(MockApi mockApi) {
+            id.setText(mockApi.getId());
+            name.setText(mockApi.getName());
+            createdAt.setText(mockApi.getCreatedAt().toString());
+
+            linearLayout.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (itemClickListener != null) {
+                itemClickListener.onItemClick(mockApiList.get(getAdapterPosition()));
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_recyclerview, parent, false);
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((RecyclerViewHolder) holder).bindData(mockApiList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mockApiList.size();
+    }
+
+    public void setData(List<MockApi> mockApiList) {
+        this.mockApiList = mockApiList;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(MockApi mockApi);
+    }
+}
--- app/src/main/java/br/com/eduardotanaka/androidrecipes/ui/retrofit/RetrofitActivity.java
+++ app/src/main/java/br/com/eduardotanaka/androidrecipes/ui/retrofit/RetrofitActivity.java
@@ -1,16 +1,21 @@
 package br.com.eduardotanaka.androidrecipes.ui.retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import br.com.eduardotanaka.androidrecipes.R;
+import br.com.eduardotanaka.androidrecipes.ui.recyclerview.CustomAdapterRV;
 import br.com.eduardotanaka.androidrecipes.ui.retrofit.model.MockApi;
 import br.com.eduardotanaka.androidrecipes.ui.retrofit.service.MockApiService;
 import retrofit2.Call;
@@ -19,15 +24,27 @@ import retrofit2.Response;
 
 import static br.com.eduardotanaka.androidrecipes.ui.retrofit.RetrofitConfig.getInstance;
 
-public class RetrofitActivity extends AppCompatActivity {
+public class RetrofitActivity extends AppCompatActivity implements CustomAdapterRV.ItemClickListener {
 
     private final String TAG = RetrofitActivity.class.getName();
     private String id;
+    private RecyclerView recyclerView;
+    private CustomAdapterRV customAdapterRV;
+    private List<MockApi> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit);
+
+        customAdapterRV = new CustomAdapterRV(this);
+        customAdapterRV.setData(new ArrayList<>());
+        recyclerView = findViewById(R.id.recyclerview);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(customAdapterRV);
     }
 
     public void getAll(View view) {
@@ -37,8 +54,12 @@ public class RetrofitActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<List<MockApi>> call, Response<List<MockApi>> response) {
                 if (response.isSuccessful()) {
-                    List<MockApi> lista = response.body();
+                    lista = new ArrayList<>();
+                    lista = response.body();
                     Log.i(TAG, lista.toString());
+
+                    customAdapterRV.setData(lista);
+                    customAdapterRV.notifyDataSetChanged();
                 } else {
                     Toast.makeText(RetrofitActivity.this, "ERRO GET ALL", Toast.LENGTH_SHORT).show();
                 }
@@ -60,6 +81,11 @@ public class RetrofitActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     MockApi mockApi = response.body();
                     Log.i(TAG, mockApi.toString());
+
+                    lista = new ArrayList<>();
+                    lista.add(mockApi);
+                    customAdapterRV.setData(lista);
+                    customAdapterRV.notifyDataSetChanged();
                 } else {
                     Toast.makeText(RetrofitActivity.this, "ERRO GET BY ID", Toast.LENGTH_SHORT).show();
                 }
@@ -86,6 +112,10 @@ public class RetrofitActivity extends AppCompatActivity {
                     MockApi mockApi = response.body();
                     id = mockApi.getId();
                     Log.i(TAG, mockApi.toString());
+
+                    lista.add(mockApi);
+                    customAdapterRV.setData(lista);
+                    customAdapterRV.notifyItemInserted(Integer.valueOf(id) - 1);
                 } else {
                     Toast.makeText(RetrofitActivity.this, "ERRO POST", Toast.LENGTH_SHORT).show();
                 }
@@ -111,6 +141,10 @@ public class RetrofitActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     MockApi mockApi = response.body();
                     Log.i(TAG, mockApi.toString());
+
+                    lista.set(Integer.valueOf(id) - 1, mockApi);
+                    customAdapterRV.setData(lista);
+                    customAdapterRV.notifyItemChanged(Integer.valueOf(id) - 1);
                 } else {
                     Toast.makeText(RetrofitActivity.this, "ERRO UPDATE", Toast.LENGTH_SHORT).show();
                 }
@@ -132,6 +166,10 @@ public class RetrofitActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     MockApi mockApi = response.body();
                     Log.i(TAG, mockApi.toString());
+
+                    lista.remove(Integer.valueOf(id) -1);
+                    customAdapterRV.setData(lista);
+                    customAdapterRV.notifyItemRemoved(Integer.valueOf(id) - 1);
                 } else {
                     Toast.makeText(RetrofitActivity.this, "ERRO DELETE", Toast.LENGTH_SHORT).show();
                 }
@@ -142,5 +180,10 @@ public class RetrofitActivity extends AppCompatActivity {
                 Toast.makeText(RetrofitActivity.this, "FAILURE DELETE", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onItemClick(MockApi mockApi) {
+        Toast.makeText(this, mockApi.toString(), Toast.LENGTH_SHORT).show();
     }
 }
