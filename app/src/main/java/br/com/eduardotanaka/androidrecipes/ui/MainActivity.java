c7ab066d689c01b868bc1228661934e56e7c5bf8
Eduardo <eduardo.s.tanaka@gmail.com>
2020-03-15 17:36:50 -0300

    bottom navigation integrado com navigation ui

--- app/src/main/java/br/com/eduardotanaka/androidrecipes/ui/MainActivity.java
+++ app/src/main/java/br/com/eduardotanaka/androidrecipes/ui/MainActivity.java
@@ -8,13 +8,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import br.com.eduardotanaka.androidrecipes.R;
 import br.com.eduardotanaka.androidrecipes.util.SharedPreferencesUtil;
 
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
+
 public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +30,20 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferencesUtil.getInstance(this);
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
         // listener para ao mudar de fragment realizar alguma mudança na activity
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
             setTitle(destination.getLabel());
             switch (destination.getId()) {
                 case R.id.login:
                     getSupportActionBar().hide();
+                    bottomNavigationView.setVisibility(View.GONE);
                     break;
                 default:
                     getSupportActionBar().show();
+                    bottomNavigationView.setVisibility(View.VISIBLE);
+                    setupWithNavController(bottomNavigationView, navController);
             }
         });
     }
